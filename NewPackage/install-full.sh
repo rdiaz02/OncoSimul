42af9db70f7a00c4210e1a5ef2ccefeba23da4c0
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2014-06-04 18:04:25 +0200

    New Reduced compiles.

--- NewPackage/install-full.sh
+++ NewPackage/install-full.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm ./Full/OncoSimulR/src/OncoSimulR.so
+rm ./Full/OncoSimulR/src/OncoSimul.o
+rm ./Full/OncoSimulR/src/symbols.rds
 R-3.1.0 CMD INSTALL ./Full/OncoSimulR
--- NewPackage/install-reduced.sh
+++ NewPackage/install-reduced.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm ./Reduced/OncoSimulR/src/OncoSimulR.so
+rm ./Reduced/OncoSimulR/src/OncoSimul.o
+rm ./Reduced/OncoSimulR/src/symbols.rds
 R-3.1.0 CMD INSTALL ./Reduced/OncoSimulR
