443f24c12e89ac909096eb99e36c6d1b26454b50
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2014-06-30 01:24:22 +0200

    done preparing new restriction code for reading restricTable

--- NewPackage/Full/ex-multimap-for-restric.cpp
+++ NewPackage/Full/ex-multimap-for-restric.cpp
@@ -3,38 +3,145 @@
 using namespace Rcpp ;
 
 
-struct Deps {
-  std::vector<int> module;
+// struct Deps {
+//   std::vector<int> module;
+//   double s;
+//   double sh;
+// };
+
+// std::multimap<int, Deps> restrictTable;
+
+
+
+struct geneDeps {
+  int typeDep; // smaller, predictable size. A lot less readable, though.
   double s;
   double sh;
+  // std::vector<int> deps;
+  std::vector< std::vector<int> > deps;
 };
 
-std::multimap<int, Deps> restrictTable;
+
+// // [[Rcpp::export]]
+// void f2(){
+//   geneDeps g1;
+//   g1.s = 0.3;
+//   g1.sh = 0.6;
+//   g1.deps.push_back(3);
+// }
+
+// // [[Rcpp::export]]
+// void f3(){
+//   std::vector<geneDeps> g3;
+//   g3.resize(4);
+//   std::cout << "g3 size " << g3.size() << std::endl;
+//   std::cout << "g3[0].sh " << g3[0].sh << std::endl;
+//   g3[0].s = 0.3;
+//   g3[0].sh = 0.6;
+//   std::cout << "g3[0].sh " << g3[0].sh << std::endl;
+
+//   std::cout << "g3[0].deps.size() " << g3[0].deps.size() << std::endl;
+//   g3[0].deps.push_back(3);
+
+//   std::cout << "g3[0].deps.size() " << g3[0].deps.size() << std::endl;
+// }
+
+// [[Rcpp::export]]
+void f4(){
+  std::vector<geneDeps> g3;
+  g3.resize(4);
+  g3[0].s = 0.3;
+  g3[0].sh = 0.6;
+  g3[0].deps.resize(2);
+  // g3[0].deps[0].push_back(std::vector<int>(3));
+  std::cout << "g3[0].deps.size() " << g3[0].deps.size() << std::endl;
+  std::cout << "g3[0].deps[0].size() " << 
+    g3[0].deps[0].size() << std::endl;
+  g3[0].deps[0].push_back(3);
+  std::cout << "g3[0].deps[0].size() " << 
+    g3[0].deps[0].size() << std::endl;
+
+  std::vector<int>vv(4, 3);
+  g3[1].deps.push_back(vv);
+  std::cout << "g3[1].deps.size() " << g3[1].deps.size() << std::endl;
+  g3[1].deps.push_back(std::vector<int>(9, 5));
+  std::cout << "g3[1].deps.size() " << g3[1].deps.size() << std::endl;
+  
+  for (auto c : g3[1].deps[0])
+    std::cout << c << ' ';
+  
+  for (auto c : g3[1].deps[1])
+    std::cout << c << ' ';
+}
+
 
 
 // [[Rcpp::export]]
-void f1() {
-  //int n = 5;
-  std::vector<int> v1;
-  for(int i = 0; i != 5; ++i) {
-    for(int j = 0; j != (i + 1); ++j)
-      v1.push_back(j);
-    Deps d1;
-    d1.module = v1;
-    d1.s = 0.1 + i;
-    d1.sh = 0.6 + i;
+void restrictTable_to_cpp(List rt) {
+  // int n = rt.size();
+  int ndeps;
+  //std::cout << "size is " << n << std::endl;
+  
+  std::vector<geneDeps> restrictTable;
+  restrictTable.resize(rt.size());
+  
+  Rcpp::List rt_element;
+  Rcpp::List parent_list;
+  Rcpp::IntegerVector module;
+  for(int i = 0; i != rt.size(); ++i) {
+    //std::cout <<"\n ****** \n i = " << i << std::endl;
 
-    restrictTable.insert(std::make_pair(i, d1));
-    v1.clear();
-    
+    rt_element = rt[i];
+    restrictTable[i].typeDep = rt_element["type"];
+    restrictTable[i].s = rt_element["s"];
+    restrictTable[i].sh = rt_element["sh"];
+    parent_list = rt_element["parent"];
+    ndeps = parent_list.size();
+
+    for(int j = 0; j != ndeps; ++j) {
+      module = as<IntegerVector>(parent_list[j]);
+      restrictTable[i].deps.push_back(Rcpp::as< std::vector<int> >(module));
+
+      // std::cout <<"\n ** \n j = " << j << std::endl;
+      // for (auto c : restrictTable[i].deps[j])
+      // 	std::cout << c << ' ';
+      // std::cout << std::endl;
+
+    }
   }
-  std::cout << "\n The multimap contains \n";
-  std::multimap<int, Deps>::iterator it;
-  for(it = restrictTable.begin(); it != restrictTable.end(); ++it ) {
-    std::cout << it->first << " => " << " Module = ";
-    for(auto vv : it->second.module) std::cout << vv << ' ';
-    std::cout << " s = " << it->second.s << "; sh = " << it->second.sh << std::endl;
-  }
-  restrictTable.clear();
+ 
 }
+
+
+
+
+
+// // [[Rcpp::export]]
+// void f1() {
+//   //int n = 5;
+//   std::vector<int> v1;
+//   for(int i = 0; i != 5; ++i) {
+//     for(int j = 0; j != (i + 1); ++j)
+//       v1.push_back(j);
+//     Deps d1;
+//     d1.module = v1;
+//     d1.s = 0.1 + i;
+//     d1.sh = 0.6 + i;
+
+//     restrictTable.insert(std::make_pair(i, d1));
+//     v1.clear();
+    
+//   }
+//   std::cout << "\n The multimap contains \n";
+//   std::multimap<int, Deps>::iterator it;
+//   for(it = restrictTable.begin(); it != restrictTable.end(); ++it ) {
+//     std::cout << it->first << " => " << " Module = ";
+//     for(auto vv : it->second.module) std::cout << vv << ' ';
+//     std::cout << " s = " << it->second.s << "; sh = " << it->second.sh << std::endl;
+//   }
+//   restrictTable.clear();
+// }
+
+
+
 
---
+++ NewPackage/Full/ex-new-restrict.R
@@ -0,0 +1,189 @@
+rt2 <- data.frame(parent = c(
+                      0, 0, 0,
+                      1,
+                      "2, 3",
+                      4,
+                      "5, 6, 7",
+                      "5, 6, 7",
+                      4,
+                      4
+                      ),
+           child = c(
+               1,
+               7,
+               3,
+               2,
+               4,
+               5,
+               8,
+               9,
+               9,
+               6),
+                  s = 0.1,
+                  sh = 0.05,
+                  type = "MN",
+                  stringsAsFactors = FALSE)
+
+rt3 <- data.frame(parent = c(
+                      0, 0, 0,
+                      1,
+                      "2, 3",
+                      4,
+                      "5, 6, 7",
+                      "5, 6, 7",
+                      4,
+                      4,
+                      10,
+                      10,
+                      10,
+                      10,
+                      10,
+                      10,
+                      10,
+                      10,
+                      11,
+                      12,
+                      "13, 14, 15",
+                      "13, 14, 15",                      
+                      "16, 17, 18",
+                      2,
+                      4,
+                      3
+                      ),
+           child = c(
+               1,
+               7,
+               3,
+               2,
+               4,
+               5,
+               8,
+               9,
+               9,
+               6,
+               11,
+               12,
+               13,
+               14,
+               15,
+               16,
+               17,
+               18,
+               16,
+               19,
+               18,
+               19,
+               19,
+               19,
+               19,
+               10
+               ),
+                  s = 0.1,
+                  sh = 0.05,
+                  type = "MN",
+                  stringsAsFactors = FALSE)
+
+
+list.of.deps <- function(x) {
+    lookupType <- c("MN" = 1, "monotone" = 1,
+                    "SM" = 2, "semimonotone" = 2)
+    if(length(x) == 1)
+        return(list(
+            child = x$child,
+            s = x$s,
+            sh = x$sh,
+            type = lookupType[x$type],
+            parent = list(
+                as.integer(unlist(strsplit(x$parent, ","))))))
+    else {
+        if(length(unique(x$s))!= 1)
+            stop("Not all s identical within a child")
+        if(length(unique(x$sh))!= 1)
+            stop("Not all sh identical within a child")
+        if(length(unique(x$type))!= 1)
+            stop("Not all type identical within a child")
+        return(list(
+            child = x$child[1],
+            s = x$s[1],
+            sh = x$sh[1],
+            type = lookupType[x$type[1]],
+            parent = lapply(strsplit(x$parent, ","), as.integer)))
+    }
+}
+
+to.long.rt <- function(rt) { 
+    srt <- rt[order(rt$child), ]
+    ## check all childs
+    if(!identical(as.integer(sort(unique(rt$child))),
+                  seq.int(max(rt$child))))
+        stop("Not all children present")
+    ## splitted <- split(srt, srt$child)
+    return(lapply(split(srt, srt$child), list.of.deps))
+}
+
+
+rt.to.cpp <- function(rt) {
+    lrt <- to.long.rt(rt)
+    restrictTable_to_cpp(lrt)
+}
+
+library(Rcpp)
+## setwd("../../")
+
+sourceCpp("ex-multimap-for-restric.cpp",
+          verbose = TRUE)
+
+## f4()
+rt.to.cpp(rt2)
+
+## turn into a list, each element of the list is the poset.
+## similar to the C++ structure I'll have.
+
+## How do I turn that into my structure inside C++?
+
+
+
+library(rbenchmark)
+
+benchmark(to.long.rt(rt2), replications = 1000) ## 3.75
+benchmark(to.long.rt(rt3), replications = 1000) ## 7.48
+
+benchmark(rt.to.cpp(rt2), replications = 1000) ## 3.8
+benchmark(rt.to.cpp(rt3), replications = 1000) ## 7.5
+## so the R to CPP, in C++ part, is less than a milisecond
+## and the R code is about 3 to 7 miliseconds. And this is
+## only done once.
+
+
+
+
+
+
+## For tests: this should fail it
+rt1 <- data.frame(parent = c(
+                      0, 0, 0,
+                      1,
+                      "2, 3",
+                      4,
+                      "5, 6, 7",
+                      "5, 6, 7",
+                      4
+                      ),
+           child = c(
+               1,
+               6,
+               7,
+               2,
+               4,
+               5,
+               8,
+               9,
+               9),
+                  s = 0.1,
+                  sh = 0.05,
+                  type = "MN",
+                  stringsAsFactors = FALSE)
+
+to.long.rt(rt1)
+
+
