2b66a519d444d745e4cbb0614c4ad1cb89ccbf4d
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-01-22 11:52:34 +0100

    testing of sampling last done

--- NewPackage/Full/OncoSimulR/tests/testthat/sample-only-last.R
+++ NewPackage/Full/OncoSimulR/tests/testthat/sample-only-last.R
@@ -21,6 +21,15 @@ mc9 <- function(poset) oncoSimulIndiv(poset, model = "McFL",
                                       keepEvery = -9)
 
 
+## A list, where each element is a list
+## with two members, both expected to give same result
+FexpectedSame <- list(
+    Bozics = list(bozic = bozic, bozic9 = bozic9),
+    Exps = list(Exp = Exp, Exp9 = Exp9),
+    McFLs = list(mc = mc, mc9 = mc9)
+)
+
+
 popsNoZero <- function(x) {
     keep <- which(x$pops.by.time[nrow(x$pops.by.time), ] > 0)
     pops <- x$pops.by.time[nrow(x$pops.by.time), keep]
@@ -28,69 +37,41 @@ popsNoZero <- function(x) {
     return(list(pops = pops, genots = genots))
 }
 
-
-the.bozics <- function(seed) {
-    set.seed(seed); b1 <- bozic(Poset)
-    set.seed(seed); b2 <- bozic9(Poset)
+runBothFuncts <- function(seed, Poset, functionPair) {
+    set.seed(seed); b1 <- functionPair[[1]](Poset)
+    set.seed(seed); b2 <- functionPair[[2]](Poset)
     return(list(all = b1, last = b2))
 }
 
-the.exps <- function(seed) {
-    set.seed(seed); b1 <- Exp(Poset)
-    set.seed(seed); b2 <- Exp9(Poset)
-    return(list(all = b1, last = b2))
-}
-
-the.mcfls <- function(seed) {
-    set.seed(seed); b1 <- mc(Poset)
-    set.seed(seed); b2 <- mc9(Poset)
-    return(list(all = b1, last = b2))
-}
-
-
-## A lot of repetition below. Could make it simpler. Not worth it now?
 
 for(i in 1:length(examplePosets)) {
     s1 <- round(runif(1) * 10000)
     Poset <- examplePosets[[i]]
-    
-    test_that("Sampling only last same for Bozic", {
-        bb <- the.bozics(s1)
-        b1 <- bb$all
-        b2 <- bb$last
-        popsGenots <- popsNoZero(b1)
-        expect_equal(b1$TotalPopSize, b2$TotalPopSize)
-        expect_equal(b1$FinalTime, b2$FinalTime)
-        expect_equal(b1$NumIter, b2$NumIter)
-        expect_equal(popsGenots$pops, b2$pops.by.time[1, ])
-        expect_equal(popsGenots$genots, b2$Genotypes)
-        rm(bb, b1, b2)
-    })
-
-    test_that("Sampling only last same for Exp", {
-        bb <- the.exps(s1)
-        b1 <- bb$all
-        b2 <- bb$last
-        popsGenots <- popsNoZero(b1)
-        expect_equal(b1$TotalPopSize, b2$TotalPopSize)
-        expect_equal(b1$FinalTime, b2$FinalTime)
-        expect_equal(b1$NumIter, b2$NumIter)
-        expect_equal(popsGenots$pops, b2$pops.by.time[1, ])
-        expect_equal(popsGenots$genots, b2$Genotypes)
-        rm(bb, b1, b2)
-    })
-    
-    test_that("Sampling only last same for Bozic", {
-        bb <- the.mcfls(s1)
-        b1 <- bb$all
-        b2 <- bb$last
-        
-        popsGenots <- popsNoZero(b1)
-        expect_equal(b1$TotalPopSize, b2$TotalPopSize)
-        expect_equal(b1$FinalTime, b2$FinalTime)
-        expect_equal(b1$NumIter, b2$NumIter)
-        expect_equal(popsGenots$pops, b2$pops.by.time[1, ])
-        expect_equal(popsGenots$genots, b2$Genotypes)
-        rm(bb, b1, b2)
-    })
+    attributes(Poset)$namePoset <- names(examplePosets)[[i]]
+    for(ffs in FexpectedSame) {
+        test_that(paste("Sampling only last same for ",
+                        paste(names(ffs), collapse = " ")), {
+                            ## comment next cat later
+                            cat(paste("\n ",
+                                      " Seed = ", s1, " ",
+                                      paste(names(ffs), collapse = " "),
+                                      ". Poset = ",
+                                      attributes(Poset)$namePoset,
+                                      "\n"))
+                            
+                            bb <- runBothFuncts(s1, Poset, ffs)
+                            b1 <- bb$all
+                            b2 <- bb$last
+                            popsGenots <- popsNoZero(b1)
+                            expect_equal(b1$TotalPopSize, b2$TotalPopSize)
+                            expect_equal(b1$FinalTime, b2$FinalTime)
+                            expect_equal(b1$NumIter, b2$NumIter)
+                            expect_equal(popsGenots$pops, b2$pops.by.time[1, ])
+                            expect_equal(popsGenots$genots, b2$Genotypes)
+                            expect_false(
+                                all(dim(b2$pops.by.time) ==
+                                        dim(b1$pops.by.time)))
+                            rm(bb, b1, b2)
+                        })   
+    }
 }
