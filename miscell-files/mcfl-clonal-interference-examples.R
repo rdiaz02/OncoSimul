e4992f9f6aadcdc67fad5627118f6ff9cdcf2e3e
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-06-19 03:31:27 +0200

    renaming files and cleanup

--- miscell-files/mcfl-clonal-interference-examples.R
+++ miscell-files/mcfl-clonal-interference-examples.R
@@ -134,3 +134,34 @@ domcf(1e-6, 0.045, 2000, 1)
 ## beautiful too
 set.seed(4)
 domcf(1e-6, 0.075, 3000, 1) 
+
+
+
+regimeClonalInterference <- function(N, mu, numDrivers, s) {
+    ## From Darayian and Shraiman, 2014, p. 914. But this is too
+    ## naive for many scenarios we deal for
+    return( (N * mu * numDrivers) > (1/log(N * s)) )
+}
+
+## FIXME: prepare a plot from that function? range of N?
+
+
+plotRegimeClonalInterference <- function(N, numDrivers, mu, s, npoints = 100) {
+    ## N is given as a two-element vector
+    ## numDrivers the number of drivers to examine
+    ns <- seq(from = N[1], to = N[2], length.out = npoints)
+    data <- sapply(numDrivers, function(x)
+                   regimeClonalInterference(ns, mu, x, s))
+
+    matplot(x = ns, y = data, type = "l", ylab = c("No", "Yes"),
+            lty = seq_along(numDrivers), col = seq_along(numDrivers),
+            axes = FALSE)
+    box()
+    axis(1)
+    axis(2, at = c(0, 1), labels = c("No", "Yes"))
+    legend(N[1], 1, numDrivers,
+           lty = seq_along(numDrivers), col = seq_along(numDrivers))
+}
+
+## do it for McFL model. Remember that a relationship between N and number
+## of drivers exists.
--- miscell-files/run-segfault.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pkill -f bioc-devel
-~/Sources/R-3.2.0-bioc-devel-68444/bin/Rscript --vanilla segf1.R
-
