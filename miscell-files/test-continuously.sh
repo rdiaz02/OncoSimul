bf32265628f8abf135f649085d9b8baf307be1e9
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-10-01 16:10:20 +0200

    shell script for test continuousl.

---
+++ miscell-files/test-continuously.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NRUNS=$1
+
+if [[ $# -ne 1 ]]; then
+   echo "Pass the number of processes as argument"
+fi
+
+   
+   
+for (( run = 1; run <=${NRUNS}; run++ ))
+do
+    R --vanilla --slave < test-continuously.R > test-continuously-$run.Rout
+done
+
