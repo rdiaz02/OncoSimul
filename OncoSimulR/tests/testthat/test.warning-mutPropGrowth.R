90afffc252179a67cee0526a226ed2dc7e92d559
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2016-03-03 11:55:18 +0100

    bozic: ensure we pass right range in tests

--- OncoSimulR/tests/testthat/test.warning-mutPropGrowth.R
+++ OncoSimulR/tests/testthat/test.warning-mutPropGrowth.R
@@ -1,7 +1,7 @@
 test_that("mutationPropGrowth warning with Bozic, indiv", {
               oi <- allFitnessEffects(orderEffects =
                c("F > D" = -0.3, "D > F" = 0.4),
-               noIntGenes = rexp(5, 10),
+               noIntGenes = runif(5, 0.01, 0.06),
                           geneToModule =
                               c("Root" = "Root",
                                 "F" = "f1, f2, f3",
@@ -34,9 +34,12 @@ test_that("mutationPropGrowth warning with Bozic, indiv", {
 ##           })
 
 test_that("mutationPropGrowth warning with Bozic, sample", {
+    ## Bozic will crash on purpose if any s >= 1. Make sure
+    ## that does not happen here
+    
               oi <- allFitnessEffects(orderEffects =
                c("F > D" = -0.3, "D > F" = 0.4),
-               noIntGenes = rexp(5, 10),
+               noIntGenes = runif(5, 0.01, 0.06),
                           geneToModule =
                               c("Root" = "Root",
                                 "F" = "f1, f2, f3",
