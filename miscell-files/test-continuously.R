468b8e378fdcbd87bc1ddd7541802705bbd9455a
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2016-03-05 11:50:18 +0100

    correct now?

--- miscell-files/test-continuously.R
+++ miscell-files/test-continuously.R
@@ -5,7 +5,7 @@ library(OncoSimulR)
 library(tools)
 ## recall to install with --install-tests
 
-if (Sys.info("nodename") == "Gallotia") {
+if (Sys.info()["nodename"] == "Gallotia") {
     td <- "/home2/ramon/tmp"
 } else {
     td <- "~/tmp"
@@ -17,8 +17,8 @@ while(TRUE) {
     i <- i + 1
     cat("\n\n Doing iteration ", i, "\n")
     cat("\n And this is the first random uniform number ", runif(1), "\n")
-    nn <- paste(sample(c(letters, 0:1), 12, replace = TRUE), collapse = "")
-    nnn <- paste0(tempfile(pattern="", tmpdir = td), nn)
+    nn <- paste(sample(c(letters, 0:1), 15, replace = TRUE), collapse = "")
+    nnn <- paste0(tempfile(pattern="Rtmp", tmpdir = td), nn)
     dir.create(nnn)
     cat(paste("Using directory ", nnn, "\n"))
     test_package("OncoSimulR") ## testthat, but if interactive, asks for plots,
