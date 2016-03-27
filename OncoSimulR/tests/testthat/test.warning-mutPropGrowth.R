18b89ce5160e2d7f13c3046af1de1f66fe6072b1
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-03-27 11:08:16 +0200

    decrease sampleEvery for one test
    Giving the "Recoverable exception ti set to DBL_MIN. Rerunning" in less
    than 1 in 10000 tests.

--- OncoSimulR/tests/testthat/test.warning-mutPropGrowth.R
+++ OncoSimulR/tests/testthat/test.warning-mutPropGrowth.R
@@ -59,7 +59,9 @@ test_that("mutationPropGrowth warning with Bozic, sample", {
 
 ## With seed 5207947 we get a Recoverable exception ti set to
 ## DBL_MIN. Rerunning. Not if we set the onlyCancer = FALSE. Also, not if
-## we use a sampleEvery = 0.1. So a rare numerical issue. OK.
+## we use a sampleEvery = 0.1. Setting it to 0.1 still same issue with
+## seed 5613635 or 465554. Again, estimate is less < 1/10000. So a rare
+## numerical issue. OK. Decrease sampleEvery further, but increase keepEvery for size.
 
 test_that("mutationPropGrowth no warning with Exp, indiv", {
     ## I once (out of over > 10000) saw it fail. Try to catch it
@@ -76,7 +78,8 @@ test_that("mutationPropGrowth no warning with Exp, indiv", {
     expect_silent(oiI1 <- oncoSimulIndiv(oi,
                                          model = "Exp",
                                          onlyCancer = TRUE,
-                                         sampleEvery = 0.1,
+                                         sampleEvery = 0.03,
+                                         keepEvery = 5,
                                          mutationPropGrowth = TRUE,
                                          seed = NULL))                
 })
