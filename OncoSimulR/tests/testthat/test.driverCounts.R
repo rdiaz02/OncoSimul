a5ed4fd344aa6fa4868de763246b4e0a2f791c94
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-04-07 17:38:46 +0200

    oops, some tests do produces messages in windows as in windows we let the user now mc.cores has no effect

--- OncoSimulR/tests/testthat/test.driverCounts.R
+++ OncoSimulR/tests/testthat/test.driverCounts.R
@@ -3,7 +3,7 @@ RNGkind("Mersenne-Twister") ## we have some examples below with random
 
 date()
 test_that("Runs without crashes", {
-    
+
     iteration <- 1
     ni <- runif(10, min = -0.01, max = 0.1)
     names(ni) <- paste0("g", 2:11)
@@ -15,7 +15,7 @@ test_that("Runs without crashes", {
         drvN <- paste0("g", i)
         fe31 <- allFitnessEffects(noIntGenes = ni,
                                   drvNames = drvN)
-        expect_silent(uu <- oncoSimulPop(20,
+        expect_output(print(oncoSimulPop(20,
                                          fe31, 
                                          mu = 1e-6,
                                          initSize = 1e5,
@@ -26,14 +26,17 @@ test_that("Runs without crashes", {
                                          onlyCancer = FALSE,
                                          mc.cores = 2,
                                          sampleEvery = 0.03
-                                         ))
+                                         )),
+                      "Population of OncoSimul trajectories",
+                      fixed = TRUE)
         iteration <- iteration + 1
     }
     ## some pairs, trios, etc, shuffled order; all are run in the long
     ## test; this is a paranoid testing set up
     ## The ones run here are the most dangerous ones.
+    require(gtools)
     for(n in c(2, 3)) {
-        m <- combinations(10, n, 2:11)
+        m <- gtools::combinations(10, n, 2:11)
         for(j in sample(nrow(m), 45)) { ## all for 2, a sample of 45 for 3
             cat("\n doing iteration", iteration, "\n")
             ni[] <- runif(10, min = -0.01, max = 0.1)
@@ -41,7 +44,7 @@ test_that("Runs without crashes", {
             drvN <- paste0("g", sample(m[j, ]))
             fe31 <- allFitnessEffects(noIntGenes = ni,
                                   drvNames = drvN)
-            expect_silent(uu <- oncoSimulPop(10,
+            expect_output(print(oncoSimulPop(10,
                                              fe31, 
                                              mu = 1e-6,
                                              initSize = 1e5,
@@ -52,7 +55,9 @@ test_that("Runs without crashes", {
                                              onlyCancer = FALSE,
                                              mc.cores = 2,
                                              sampleEvery = 0.03
-                                             ))
+                                             )),
+                          "Population of OncoSimul trajectories",
+                          fixed = TRUE)
             iteration <- iteration + 1
         }
     }
