449d4338b663f42640a46ecb3a3645a8385496bd
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-04-09 18:52:16 +0200

    2.1.5: show one output of driverCounts test

--- OncoSimulR/tests/testthat/test.driverCounts.R
+++ OncoSimulR/tests/testthat/test.driverCounts.R
@@ -68,6 +68,9 @@ date()
 
 date()
 test_that("Assertion is correct when nothing returned",{
+    ## Fixing the seed here isn't really needed. These are just a few
+    ## specific cases in Linux, but other compilers will give different
+    ## runs. None should fail, though.
     RNGkind("L'Ecuyer-CMRG") 
     set.seed(13)
     oi <- allFitnessEffects(orderEffects =
@@ -113,5 +116,25 @@ test_that("Assertion is correct when nothing returned",{
 } )
 date()
 
+
+## Just to check no crashes and to show output
+date()
+test_that("driverCounts: a run that used to cause crashes", {
+    set.seed(1)
+    ni <- runif(10, min = -0.01, max = 0.1)
+    names(ni) <- paste0("g", 2:11)
+    fe31 <- allFitnessEffects(noIntGenes = ni,
+                              drvNames = "g2") 
+    set.seed(1)
+    mue11 <- oncoSimulIndiv(fe31, 
+                            mu = 1e-6,
+                            initSize = 1e5,
+                            model = "McFL",
+                            detectionSize = 5e6,
+                            finalTime = 5, 
+                            onlyCancer = FALSE)
+    cat("\n ... output from mue11\n")
+    print(mue11)
+})
 
 cat(paste("\n Ending driverCounts at", date(), "\n"))
