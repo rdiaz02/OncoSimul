8eaf1612d5080adc7f76a14d8ef72c96294d4b18
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2016-04-13 17:23:02 +0200

    more test reorganization

--- OncoSimulR/tests/manual/test.warning-mutPropGrowth-long.R
+++ OncoSimulR/tests/manual/test.warning-mutPropGrowth-long.R
@@ -41,4 +41,54 @@ test_that("mutationPropGrowth OK  with McFL, sample", {
                              "Successfully", fixed = TRUE)
 })
 
+
+## test_that("mutationPropGrowth warning with Bozic, population", {
+##     ## Will not return the warning. Seems a bug in mclapply not respecting
+##     ## mc.silent.
+##     ## http://stackoverflow.com/questions/21486658/warnings-suppressed-with-mclapply-in-r
+##               oi <- allFitnessEffects(orderEffects =
+##                c("F > D" = -0.3, "D > F" = 0.4),
+##                noIntGenes = rexp(5, 10),
+##                           geneToModule =
+##                               c("Root" = "Root",
+##                                 "F" = "f1, f2, f3",
+##                                 "D" = "d1, d2") )
+##               expect_warning(oiI1 <- oncoSimulPop(4,
+##                                                   oi,
+##                                                   model = "Bozic",
+##                                                   mutationPropGrowth = TRUE),
+##                              "Using fitness Bozic (bozic1) with mutationPropGrowth = TRUE;",
+##                              fixed = TRUE)                                                     
+##           })
+
+test_that("mutationPropGrowth warning with Bozic, sample", {
+    ## Bozic will crash on purpose if any s >= 1. Make sure
+    ## that does not happen here
+
+    RNGkind("L'Ecuyer-CMRG") ## for the mclapplies
+    
+    RNGkind("Mersenne-Twister")
+    set.seed(13)
+    oi <- allFitnessEffects(orderEffects =
+                                c("F > D" = -0.3, "D > F" = 0.4),
+                            noIntGenes = runif(5, 0.01, 0.06),
+                            geneToModule =
+                                c("Root" = "Root",
+                                  "F" = "f1, f2, f3",
+                                  "D" = "d1, d2"),
+                            drvNames = c("d1", "d2", "f1", "f2", "f3"))
+    
+    set.seed(10)
+    expect_warning(oiI1 <- oncoSimulSample(4,
+                                           oi,
+                                           sampleEvery = 0.03,
+                                           onlyCancer = FALSE,
+                                           model = "Bozic",
+                                           mutationPropGrowth = TRUE,
+                                           seed = NULL),
+                   "Using fitness Bozic (bozic1) with mutationPropGrowth = TRUE;",
+                   fixed = TRUE)
+    
+})
+
 cat(paste("\n Ending warning-mutPropGrowth-long tests", date(), "\n"))
