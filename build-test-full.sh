25b4afb55e45ad59fbe67f997badf10f84e68ebd
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2016-04-13 16:08:15 +0200

    cleaning up before building

--- build-test-full.sh
+++ build-test-full.sh
@@ -6,6 +6,30 @@ V_ADA=$(cat ./OncoSimulR/DESCRIPTION | grep Version | cut -d' ' -f2)
 
 rm OncoSimulR_$V_ADA.tar.gz
 
+rm ./OncoSimulR/Rplots.pdf
+rm ./OncoSimulR/*~
+rm ./OncoSimulR/inst/*~
+rm ./OncoSimulR/man/*~
+rm ./OncoSimulR/man/Rplots.pdf
+rm ./OncoSimulR/man/.Rhistory
+rm ./OncoSimulR/tests/*~
+rm ./OncoSimulR/tests/Rplots.pdf
+rm ./OncoSimulR/tests/.Rhistory
+rm ./OncoSimulR/data/*~
+rm ./OncoSimulR/data/Rplots.pdf
+rm ./OncoSimulR/data/.Rhistory
+rm ./OncoSimulR/tests/testthat/*~
+rm ./OncoSimulR/tests/testthat/Rplots.pdf
+rm ./OncoSimulR/tests/testthat/.Rhistory
+rm ./OncoSimulR/tests/manual/*~
+rm ./OncoSimulR/tests/manual/Rplots.pdf
+rm ./OncoSimulR/tests/manual/.Rhistory
+rm ./OncoSimulR/R/*~
+rm ./OncoSimulR/R/.Rhistory
+rm ./OncoSimulR/R/Rplots.pdf
+rm ./OncoSimulR/vignettes/*~
+rm ./OncoSimulR/vignettes/Rplots.pdf
+rm ./OncoSimulR/vignettes/.Rhistory
 rm ./OncoSimulR/src/*.so
 rm ./OncoSimulR/src/*.o
 rm ./OncoSimulR/src/OncoSimulR.so
