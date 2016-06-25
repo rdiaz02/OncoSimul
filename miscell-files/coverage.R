278bcc64dc3a1f557f93c111d399d5365e300845
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-06-25 16:44:14 +0200

    oops

--- miscell-files/coverage.R
+++ miscell-files/coverage.R
@@ -17,6 +17,6 @@ cov3 <- package_coverage(type = "all", combine_types = FALSE, quiet = FALSE)
 cov2 <- package_coverage(type = "all", combine_types = TRUE, quiet = FALSE)
 
 ## cov4 <- package_coverage()
-save(file = "../miscell-files/coverage-results.RData", cov4, cov3)
+save(file = "../miscell-files/coverage-results.RData", cov4, cov3, cov2)
 ## zero_coverage(cov4) 
 shine(cov4)
