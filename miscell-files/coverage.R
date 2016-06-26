98ba3f047760c8cfdcb046c6ae63a7da393f9119
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-06-26 11:31:49 +0200

    covr with and without -O0/-O3

--- miscell-files/coverage.R
+++ miscell-files/coverage.R
@@ -1,6 +1,16 @@
 ## Run the coverage script
 
 library(covr)
+
+## Make sure C++-11 code also covered.
+## See https://github.com/jimhester/covr/issues/198#issuecomment-228568000
+options(covr.flags = c(CPPFLAGS = "-O0 -g --coverage -fno-inline -fno-elide-constructors",
+                       CXX1XFLAGS = "-O0 -g --coverage -fno-inline -fno-elide-constructors",
+                       CXXFLAGS = "-O0 -g --coverage -fno-inline -fno-elide-constructors",
+                       CFLAGS = "-O0 -g --coverage -fno-inline -fno-elide-constructors",
+                       LDFLAGS = "--coverage -fno-elide-constructors"))
+
+
 library(shiny)
 library(DT)
 library(OncoSimulR)
@@ -12,12 +22,39 @@ setwd("../OncoSimulR")
 ## What matters most is tests after all. And
 ## running all and not combining (or one of them) screws up
 ## navigating to the code
-cov4 <- package_coverage(type = "tests", quiet = FALSE)
 
-cov3 <- package_coverage(type = "all", combine_types = FALSE, quiet = FALSE)
-cov2 <- package_coverage(type = "all", combine_types = TRUE, quiet = FALSE)
+
+## Make sure C++-11 code also covered.
+## See https://github.com/jimhester/covr/issues/198#issuecomment-228568000
+options(covr.flags = c(CPPFLAGS = "-O0 -g --coverage -fno-default-inline -fno-inline -fno-elide-constructors",
+                       CXX1XFLAGS = "-O0 -g --coverage -fno-default-inline -fno-inline -fno-elide-constructors",
+                       CXXFLAGS = "-O0 -g --coverage -fno-default-inline -fno-inline -fno-elide-constructors",
+                       CFLAGS = "-O0 -g --coverage -fno-default-inline -fno-inline -fno-elide-constructors",
+                       LDFLAGS = "--coverage -fno-elide-constructors"))
+
+cov_O0 <- package_coverage(type = "tests", quiet = FALSE)
+
+## But I run with -O3, functions are inlined, etc, so paths could
+## differ. Do it also this way. And this deals correctly (almost, not
+## fully) with the .h files and their structs.
+
+options(covr.flags = c(CPPFLAGS = "-O3 -g --coverage",
+                       CXX1XFLAGS = "-O3 -g --coverage",
+                       CXXFLAGS = "-O3 -g --coverage",
+                       CFLAGS = "-O3 -g --coverage",
+                       LDFLAGS = "--coverage"))
+
+cov_O3 <- package_coverage(type = "tests", quiet = FALSE)
+
+save(file = "../miscell-files/coverage-results.RData", cov_O0, cov_O3)
+
+
+## cov3 <- package_coverage(type = "all", combine_types = FALSE, quiet = FALSE)
+## cov2 <- package_coverage(type = "all", combine_types = TRUE, quiet = FALSE)
 
 ## cov4 <- package_coverage()
-save(file = "../miscell-files/coverage-results.RData", cov4, cov3, cov2)
+## save(file = "../miscell-files/coverage-results.RData", cov4, cov3, cov2)
 ## zero_coverage(cov4) 
-shine(cov4)
+shine(cov_O0)
+
+shine(cov_O3)
