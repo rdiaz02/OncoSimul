3bc493f2d8c20514cb9f250ba93e3f430713c267
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2016-07-01 00:16:49 +0200

    fixed two tests that now fail occasionally explicit non-prob-exiting

--- OncoSimulR/tests/testthat/test.init-mutant.R
+++ OncoSimulR/tests/testthat/test.init-mutant.R
@@ -459,6 +459,7 @@ test_that("initMutant with oncoSimulPop, McFL", {
                         keepPhylog = TRUE,
                         sampleEvery = 0.03,
                         keepEvery = 1,
+                        n2 = NULL, p2 = NULL, cPDetect = NULL,
                         initSize = 500,
                         initMutant = c("d > m > y"),
                         mc.cores = 2
