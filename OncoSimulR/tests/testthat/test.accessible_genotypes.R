770c38fe5bff004c9a2f9882909d1c57a79b44b7
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2016-11-03 11:27:20 +0100

    qualify namespace fully

--- OncoSimulR/tests/testthat/test.accessible_genotypes.R
+++ OncoSimulR/tests/testthat/test.accessible_genotypes.R
@@ -8,7 +8,7 @@ test_that("We obtain same accessible genotypes with different functions", {
             a1 <- OncoSimulR:::faster_accessible_genotypes_R(rtmp, 0)
             ajm <- OncoSimulR:::genot_to_adj_mat(rtmp)
             a2 <- colnames(OncoSimulR:::filter_inaccessible(ajm, 0))
-            a3 <- wrap_accessibleGenotypes(rtmp, 0)
+            a3 <- OncoSimulR:::wrap_accessibleGenotypes(rtmp, 0)
             stopifnot(identical(as.integer(a1), a3))
             stopifnot(identical(as.integer(a2), a3))
             stopifnot(all(a2 ==  a3))
