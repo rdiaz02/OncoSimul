258df9eac914edefd15acdc56f5ac756d6bea80a
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2016-12-11 17:54:16 +0100

    trying to get both html and pdf tables renderd OK

--- pdf-vignette.sh
+++ pdf-vignette.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 ## simplify my workflow.
 cd ./OncoSimulR/vignettes
-Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE))'
+
+grep -v -F "Table: (\#tab:" OncoSimulR.Rmd > OncoSimulR-tex.Rmd
+sed -i 's/## caption = "\\\\label/caption = "\\\\label/' OncoSimulR-tex.Rmd
+sed -i 's/## panderOptions("table.split.cells", 8)/panderOptions("table.split.cells", 8)/' OncoSimulR-tex.Rmd
+
+Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR-tex.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE))'
+
+cp OncoSimulR-tex.pdf OncoSimulR.pdf
+
 cd ../..
