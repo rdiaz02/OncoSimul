4007a1ce20f4773d249ed473451d59ae3994fc07
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2016-12-12 12:04:52 +0100

    explain the grep

--- pdf-vignette.sh
+++ pdf-vignette.sh
@@ -2,6 +2,10 @@
 ## simplify my workflow.
 cd ./OncoSimulR/vignettes
 
+## Beware that the first table, the one based on GSR, should
+## not be caught by this grep. In that table there is no
+## space after ":" --- could use a different convention
+## and name all others as "tab:tabtab" or something.
 grep -v -F "Table: (\#tab:" OncoSimulR.Rmd > OncoSimulR-tex.Rmd
 sed -i 's/## caption = "\\\\label/caption = "\\\\label/' OncoSimulR-tex.Rmd
 sed -i 's/## panderOptions("table.split.cells", 8)/panderOptions("table.split.cells", 8)/' OncoSimulR-tex.Rmd
