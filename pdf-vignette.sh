1031e697f4a920ae6120c78b3c1f3e818ffcad53
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2016-12-13 20:38:33 +0100

    more changes to vignette; formatting, etc

--- pdf-vignette.sh
+++ pdf-vignette.sh
@@ -9,6 +9,8 @@ cd ./OncoSimulR/vignettes
 grep -v -F "Table: (\#tab:" OncoSimulR.Rmd > OncoSimulR-tex.Rmd
 sed -i 's/## caption = "\\\\label/caption = "\\\\label/' OncoSimulR-tex.Rmd
 sed -i 's/## panderOptions("table.split.cells", 8)/panderOptions("table.split.cells", 8)/' OncoSimulR-tex.Rmd
+sed -i 's/## panderOptions("table.split.cells", 12)/panderOptions("table.split.cells", 12)/' OncoSimulR-tex.Rmd
+sed -i 's/## panderOptions("table.split.cells", 15)/panderOptions("table.split.cells", 15)/' OncoSimulR-tex.Rmd
 
 Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR-tex.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE))'
 
