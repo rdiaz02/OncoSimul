5244c96ca51054881cb7fdaeafee4a9b57f7a377
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2017-11-22 00:17:47 +0100

    tests LOD

--- OncoSimulR/tests/testthat/test.LOD_POM.R
+++ OncoSimulR/tests/testthat/test.LOD_POM.R
@@ -88,22 +88,90 @@ test_that("Warnings when no descendants",  {
                    fixed = TRUE)
 })
 
+## Done better below and make sure it runs with keepPhylog = FALSE
+## date()
+## test_that("LOD, strict, same as would be obtained from full structure", {
+##     ## we are testing in an extremely paranoid way, against a
+##     ## former version
+##     n <- 10
+##     for(i in 1:n) {
+##         ng <- 6
+##         rxx <- rfitness(ng)
+##         rxx[sample(2:(ng + 1)), ng + 1] <- 1.5 ## make sure we get going
+##         s7 <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
+##                              initSize = 1000, detectionSize = 1e6,
+##                              keepPhylog = TRUE, mu = 1e-3)
+##         lods <- LOD(s7)
+##         loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7, strict = FALSE)
+##         ## lods should be among the loda
+##         if(!is.null(s7$pops.by.time)) {
+##             expect_true(any(
+##                 unlist(lapply(loda$all_paths,
+##                               function(x) identical(names(x),
+##                                                     names(lods))))))
+##             if(length(loda$all_paths) == 1) {
+##                 expect_true(identical(names(loda$lod_single),
+##                                       names(lods)))
+##             }
+##             ## print(OncoSimulR:::LOD_as_path(lods))
+##             ## print(OncoSimulR:::LOD_as_path(loda))
+##         }
+##     }
+## })
+## date()
 
+## Done better below and make sure it runs with keepPhylog = FALSE
+## date()
+## test_that("LOD, strict, same as would be obtained from full structure, with initMutant", {
+##     n <- 10
+##     ## with initMutant
+##     for(i in 1:n) {
+##         rxx <- rfitness(6)
+##         rxx[3, 7] <- 1.5        
+##         s7 <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
+##                              initSize = 1000, detectionSize = 1e6,
+##                              keepPhylog = TRUE, mu = 1e-3,
+##                              initMutant = c("B"))
+##         lods <- LOD(s7)
+##         loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7, strict = FALSE)
+##         ## lods should be among the loda
+##         expect_true(any(
+##             unlist(lapply(loda$all_paths,
+##                           function(x) identical(names(x),
+##                                                 names(lods))))))
+##         if(length(loda$all_paths) == 1) {
+##             expect_true(identical(names(loda$lod_single),
+##                                   names(lods)))
+##         }
+##         ## print(loda)
+##     }
+## })
+## date()
+
+set.seed(NULL)
+si <- runif(1, 1, 1e9)
+print(si)
 date()
-test_that("LOD, strict, same as would be obtained from full structure", {
+test_that("LOD, strict, same as would be obtained from full structure, seed", {
     ## we are testing in an extremely paranoid way, against a
     ## former version
-
     n <- 10
     for(i in 1:n) {
-        ng <- 6
-        rxx <- rfitness(ng)
-        rxx[sample(2:(ng + 1)), ng + 1] <- 1.5 ## make sure we get going
+        ng <- 8
+        rxx <- rfitness(ng, c = 1, sd = 0.1,
+                        reference = rep(1, ng))
+        ## rxx[sample(2:(ng + 1)), ng + 1] <- 1.5 ## make sure we get going
+        set.seed(si + i)
         s7 <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
-                             initSize = 1000, detectionSize = 1e6,
-                             keepPhylog = TRUE, mu = 1e-3)
+                             initSize = 10, detectionSize = 1e5,
+                             keepPhylog = FALSE, mu = 5e-3)
+        set.seed(si + i)
+        s7b <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
+                             initSize = 10, detectionSize = 1e5,
+                             keepPhylog = TRUE, mu = 5e-3)
         lods <- LOD(s7)
-        loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7, strict = FALSE)
+        print(lods)
+        loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7b, strict = FALSE)
         ## lods should be among the loda
         if(!is.null(s7$pops.by.time)) {
             expect_true(any(
@@ -118,25 +186,32 @@ test_that("LOD, strict, same as would be obtained from full structure", {
             ## print(OncoSimulR:::LOD_as_path(loda))
         }
     }
-
-    
 })
-
 date()
 
+set.seed(NULL)
+si <- runif(1, 1, 1e9)
+print(si)
 date()
 test_that("LOD, strict, same as would be obtained from full structure, with initMutant", {
-    n <- 100
+    n <- 10
     ## with initMutant
     for(i in 1:n) {
-        rxx <- rfitness(6)
-        rxx[3, 7] <- 1.5        
+        rxx <- rfitness(8, reference = rep(1, 8))
+        rxx[4, ncol(rxx)] <- 1.5
+        set.seed(2 * si + i)
         s7 <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
-                             initSize = 1000, detectionSize = 1e6,
-                             keepPhylog = TRUE, mu = 1e-3,
-                             initMutant = c("B"))
+                             initSize = 100, detectionSize = 1e5,
+                             keepPhylog = FALSE, mu = 5e-3,
+                             initMutant = c("C"))
+        set.seed(2 * si + i)
+        s7b <- oncoSimulIndiv(allFitnessEffects(genotFitness = rxx),
+                             initSize = 100, detectionSize = 1e5,
+                             keepPhylog = TRUE, mu = 5e-3,
+                             initMutant = c("C"))
         lods <- LOD(s7)
-        loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7, strict = FALSE)
+        print(lods)
+        loda <- OncoSimulR:::LOD.oncosimul2_pre_2.9.2(s7b, strict = FALSE)
         ## lods should be among the loda
         expect_true(any(
             unlist(lapply(loda$all_paths,
@@ -150,6 +225,8 @@ test_that("LOD, strict, same as would be obtained from full structure, with init
     }
 })
 date()
+set.seed(NULL)
+
 
 date()
 test_that("POM from C++ is the same as from the pops.by.time object", {
@@ -204,83 +281,83 @@ date()
 
 
 
-## To see how the above works by returning LOD sensu stricto you can look
-## at this code:
+## ## To see how the above works by returning LOD sensu stricto you can look
+## ## at this code:
 
 
 
-pancr <- allFitnessEffects(data.frame(parent = c("Root", rep("KRAS", 4), "SMAD4", "CDNK2A", 
-                                               "TP53", "TP53", "MLL3"),
-                                           child = c("KRAS","SMAD4", "CDNK2A", 
-                                               "TP53", "MLL3",
-                                               rep("PXDN", 3), rep("TGFBR2", 2)),
-                                           s = 0.05,
-                                           sh = -0.3,
-                                           typeDep = "MN"))
+## pancr <- allFitnessEffects(data.frame(parent = c("Root", rep("KRAS", 4), "SMAD4", "CDNK2A", 
+##                                                "TP53", "TP53", "MLL3"),
+##                                            child = c("KRAS","SMAD4", "CDNK2A", 
+##                                                "TP53", "MLL3",
+##                                                rep("PXDN", 3), rep("TGFBR2", 2)),
+##                                            s = 0.05,
+##                                            sh = -0.3,
+##                                            typeDep = "MN"))
      
      
-pancr1 <- oncoSimulIndiv(pancr, model = "Exp", keepPhylog = TRUE)
+## pancr1 <- oncoSimulIndiv(pancr, model = "Exp", keepPhylog = TRUE)
 
-## All we need to get LOD sensu stricto (i.e., identical to Szendro)
-## is keep pop size of receiving, or destination, genotype.
-## Then, filter those where popSize > 0
-
-
-## And use first (starting from bottom) of that path
-## So find, for each child, the last event with popSize == 0,
-## and keep only that row.
-
-## Probably enough to run duplicated in reverse (on the df with popSize
-## child = 0)
-
-## The indices to keep: !rev(duplicated(rev(fg3[, 2])))
-
-fg1 <- pancr1$other$PhylogDF
+## ## All we need to get LOD sensu stricto (i.e., identical to Szendro)
+## ## is keep pop size of receiving, or destination, genotype.
+## ## Then, filter those where popSize > 0
 
 
-fg3 <- data.frame(parent = c("", "", "A", "B", "C", "A, B"),
-                  child =  c("A","B","A, B","A, B", "A, C", "A, B, C"),
-                  time = 1:6,
-                  pop_size_child = c(0, 0, 0, 0, 0, 0),
-                  stringsAsFactors = FALSE)
+## ## And use first (starting from bottom) of that path
+## ## So find, for each child, the last event with popSize == 0,
+## ## and keep only that row.
+
+## ## Probably enough to run duplicated in reverse (on the df with popSize
+## ## child = 0)
+
+## ## The indices to keep: !rev(duplicated(rev(fg3[, 2])))
+
+## fg1 <- pancr1$other$PhylogDF
 
 
-fg4 <- data.frame(parent = c("", "", "A", "B", "C", "A, B"),
-                  child =  c("A","B","A, B","A, B", "A, C", "A, B, C"),
-                  time = 1:6,
-                  pop_size_child = c(0, 0, 0, 2, 0, 0),
-                  stringsAsFactors = FALSE)
-
-## ## from phylogClone, key parts
-## fpc <- function(df) {
-##     tG <- unique(c(df[, 1], df[, 2]))
-##     g <- igraph::graph.data.frame(df[, c(1, 2)])
-##     nodesInP <- unique(unlist(igraph::neighborhood(g, order = 1e+09, 
-##                                                    nodes = tG, mode = "in")))
-##     allLabels <- unique(as.character(unlist(df[, c(1, 2)])))
-##     nodesRm <- setdiff(allLabels, V(g)$name[nodesInP])
-##     g <- igraph::delete.vertices(g, nodesRm)
-##     tmp <- list(graph = g, df = df)
-##     class(tmp) <- c(class(tmp), "phylogClone")
-##     return(tmp)
-## }
-
-## ## Filter the PhylogDF so we obtain LOD, sensu stricto.
-## filter_phylog_df_LOD_ <- function(x) {
-##     x <- x[x$pop_size_child == 0, ]
-##     keep <- !rev(duplicated(rev(x$child)))
-##     return(x[keep, ])
-## }
-
-require(igraph)
-all_simple_paths(OncoSimulR:::phcl_from_lod(OncoSimulR:::filter_phylog_df_LOD_with_n(fg3))$graph,
-                 from = "",
-                 to = "A, B, C",
-                 mode = "out")
+## fg3 <- data.frame(parent = c("", "", "A", "B", "C", "A, B"),
+##                   child =  c("A","B","A, B","A, B", "A, C", "A, B, C"),
+##                   time = 1:6,
+##                   pop_size_child = c(0, 0, 0, 0, 0, 0),
+##                   stringsAsFactors = FALSE)
 
 
-all_simple_paths(OncoSimulR:::phcl_from_lod(OncoSimulR:::filter_phylog_df_LOD_with_n(fg3))$graph,
-                 to = "",
-                 from = "A, B, C",
-                 mode = "in")
+## fg4 <- data.frame(parent = c("", "", "A", "B", "C", "A, B"),
+##                   child =  c("A","B","A, B","A, B", "A, C", "A, B, C"),
+##                   time = 1:6,
+##                   pop_size_child = c(0, 0, 0, 2, 0, 0),
+##                   stringsAsFactors = FALSE)
+
+## ## ## from phylogClone, key parts
+## ## fpc <- function(df) {
+## ##     tG <- unique(c(df[, 1], df[, 2]))
+## ##     g <- igraph::graph.data.frame(df[, c(1, 2)])
+## ##     nodesInP <- unique(unlist(igraph::neighborhood(g, order = 1e+09, 
+## ##                                                    nodes = tG, mode = "in")))
+## ##     allLabels <- unique(as.character(unlist(df[, c(1, 2)])))
+## ##     nodesRm <- setdiff(allLabels, V(g)$name[nodesInP])
+## ##     g <- igraph::delete.vertices(g, nodesRm)
+## ##     tmp <- list(graph = g, df = df)
+## ##     class(tmp) <- c(class(tmp), "phylogClone")
+## ##     return(tmp)
+## ## }
+
+## ## ## Filter the PhylogDF so we obtain LOD, sensu stricto.
+## ## filter_phylog_df_LOD_ <- function(x) {
+## ##     x <- x[x$pop_size_child == 0, ]
+## ##     keep <- !rev(duplicated(rev(x$child)))
+## ##     return(x[keep, ])
+## ## }
+
+## require(igraph)
+## all_simple_paths(OncoSimulR:::phcl_from_lod(OncoSimulR:::filter_phylog_df_LOD_with_n(fg3))$graph,
+##                  from = "",
+##                  to = "A, B, C",
+##                  mode = "out")
+
+
+## all_simple_paths(OncoSimulR:::phcl_from_lod(OncoSimulR:::filter_phylog_df_LOD_with_n(fg3))$graph,
+##                  to = "",
+##                  from = "A, B, C",
+##                  mode = "in")
 
