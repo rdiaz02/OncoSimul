c64775826376430beadcb660ce1739964c7f6fe0
sersancar <sergiosanchezcarrillo@gmail.com>
2018-10-25 17:17:00 +0200

    Remove old  directory

--- OncoSimul/ex-flfast.R
+++
@@ -1,8 +0,0 @@
-
-
-df1 <- data.frame(Genotype = c("WT", "A", "B, C"), Fitness = c(5, 1.3, 2),
-                  stringsAsFactors = FALSE)
-OncoSimulR:::allGenotypes_to_matrix(df1)
-
-set.seed(4)
-(r4 <- rfitness(4))
--- OncoSimul/miscell-files/AParramon_discrete_time/OncoSimulR_discrete_time_2016/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-# library(OncoSimulR)
-
-test_check("OncoSimulR")
