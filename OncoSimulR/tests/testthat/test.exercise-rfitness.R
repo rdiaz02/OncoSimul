e221a8e1a55ca2ff8c4f685cfd0f2a4dee92facd
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2020-01-22 14:20:45 +0100

    changes to pull request #16

--- OncoSimulR/tests/testthat/test.exercise-rfitness.R
+++ OncoSimulR/tests/testthat/test.exercise-rfitness.R
@@ -57,6 +57,49 @@ test_that("Error if wrong arguments", {
                  fixed = TRUE)
 })
 
+test_that("Additive, Ising, Eggbox, Full exercising", {
+
+    expect_output(print(rfitness(4, model = "Ising")), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Eggbox")), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Full")), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Additive")), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Ising", i = 0.0002, I = 0.5,
+                             circular = TRUE)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Ising", i = 2, I = 0,
+                             circular = TRUE)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Ising", i = 2, I = -2,
+                             circular = TRUE)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Ising", i = 2, I = 0.5,
+                             circular = FALSE)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Eggbox", e = 2, E = 0)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Eggbox", e = 2, E = 2.4)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Eggbox", e = 0, E = 2.4)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Full", i = 0.002, I = 2,
+                K = 2, r = TRUE,
+                p = 0.2, P = 0.3, o = 0.3, O = 1)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Full", K = 5)), "Fitness",
+              fixed = TRUE)
+expect_output(print(rfitness(4, model = "Full", i = 0.002, I = 2,
+                K = 2, r = TRUE,
+                p = 0.2, P = 0.3, o = 0.3, O = 1,
+                s = 0.5, S=0, d = 0.1,
+                e = 1, E = 1.5,
+                H = 0.5)), "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Additive", mu = 0, sd = 1)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Additive", mu = 1, sd = 0)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Additive", mu = 3, sd = 1)),
+              "Fitness", fixed = TRUE)
+expect_output(print(rfitness(4, model = "Additive", mu = -4, sd = 10)),
+              "Fitness", fixed = TRUE)
+
+})
+
+
 cat(paste("\n Ending exercise-rfitness at", date(), "\n"))
 cat(paste("  Took ", round(difftime(Sys.time(), inittime, units = "secs"), 2), "\n\n"))
 rm(inittime)
