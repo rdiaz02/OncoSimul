5025c727d80d12a985f30c2969d877c5bf460087
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2020-01-28 11:18:32 +0100

    notes on other latex engines

--- pdf-vignette.sh
+++ pdf-vignette.sh
@@ -22,6 +22,15 @@ sed -i 's/## panderOptions("table.split.cells", 15)/panderOptions("table.split.c
 sed -i 's/relfunct.png/relfunct.pdf/' OncoSimulR-tex.Rmd
 sed -i 's/hurlbut.png/hurlbut.pdf/' OncoSimulR-tex.Rmd
 
+
+## Using lualatex allows us to deal with some symbols, but messes other things
+## such as a simple balbalba$\alpha$
+## and if the symbols are used in a figure, then we get the PDF with
+## incorrect symbols a warnings.
+## So avoid it
+## Using xelatex lead to problems in other places, like tables: \blandscape
+## Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR-tex.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE, latex_engine = "lualatex"))'
+## Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR-tex.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE, latex_engine = "xelatex"))'
 Rscript -e 'library(rmarkdown); library(BiocStyle); library(bookdown); render("OncoSimulR-tex.Rmd", output_format = bookdown::pdf_document2(toc = TRUE, toc_depth = 4, keep_tex = TRUE))'
 
 cp OncoSimulR-tex.pdf OncoSimulR.pdf
