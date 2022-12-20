e1f3ceb641d239bd6cf0ef1134fd7ec127f91456
GERMAN00VP <vallejopalma.g@gmail.com>
2022-12-20 21:43:59 +0100

    Update SIMULACIONES DE PRUEBA.R

--- OncoSimulR/R/SIMULACIONES DE PRUEBA.R
+++ OncoSimulR/R/SIMULACIONES DE PRUEBA.R
@@ -5,16 +5,20 @@ library(OncoSimulR)
 # 1º Crear un objeto allFitnessEffects frequencyDependentFitnes=TRUE
 
 # a: tasa crecimiento bacterias
-a <- 10
+a <- 1.5
 # b: tasa de "muerte" fagos
-b <- 50
+b <- 5
 # c: tasa de infección por fagos
-c <- 0.01
+c <- 0.00001
 # d: tasa de infección por fago y replicación
-d <- 500
+d <- 500000*c
 
 
-genotipes_and_formulas <- data.frame("GENOTYPE"=c("B","F"),"FITNESS"=c("f_B*10-f_B*f_F*0.01","f_B*f_F*500-f_F*50"))
+genotipes_and_formulas <- data.frame("GENOTYPE"=c("B","F"),
+                                     "FITNESS"=c(paste("f_B*",a,"-f_B*f_F*",c),
+                                                 paste("f_B*f_F*",d,"-f_F*",b)))
 
-fitnessbac_fag <- allFitnessEffects(genotFitness = genotipes_and_formulas, frequencyDependentFitness = TRUE,frequencyType = "rel")
-evalAllGenotypes(fitnessbac_fag,spPopSizes = c(100000,1000),model="exp",addwt=TRUE)
+fitnessbac_fag <- allFitnessEffects(genotFitness = genotipes_and_formulas,
+                                    frequencyDependentFitness = TRUE,
+                                    frequencyType = "rel")
+evalGenotype("B",fitnessbac_fag,spPopSizes = c("B"=1,"F"=1))
