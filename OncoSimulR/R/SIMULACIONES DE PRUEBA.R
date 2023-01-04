e6ccd05479348df82b7dfd372e69299dda53c7d9
GERMAN00VP <vallejopalma.g@gmail.com>
2023-01-04 12:48:53 +0100

    Update SIMULACIONES DE PRUEBA.R

--- OncoSimulR/R/SIMULACIONES DE PRUEBA.R
+++ OncoSimulR/R/SIMULACIONES DE PRUEBA.R
@@ -1,101 +1,87 @@
-#SE TIENEN 4 POBLACIONES WT, A, B, C 
-dfat <- data.frame(Genotype = c("WT","A", "B", "C"),
-                   Fitness = c("0*n_+2.2",
-                               "2",
-                               "2",
-                               "2"))
+# Create an allFitnesEffects object with a WT and a resistant population.
+library(OncoSimulR)
+genotFitness <- data.frame("genotype"= c("WT","R","R2"),
+                           "fitness"= c("1.11", "1.05", "1.05+0*n_"),
+                           stringsAsFactors = FALSE)
 
-afe2 <- allFitnessEffects(genotFitness = dfat,
-                          frequencyDependentFitness = TRUE,
-                          frequencyType = "abs")
-
-evalAllGenotypes(afe2,spPopSizes = c(900,33,33,33))
-
-
+afe <- allFitnessEffects(genotFitness = genotFitness, 
+                         frequencyDependentFitness = TRUE,
+                         frequencyType = "abs")
 intervenciones <- list(
   list(ID="ANTIBIOTICO SOBRE WT",
-       Trigger       = "T>1",
-       WhatHappens   = "n_ = n_ -x*n_*a0 -y*n_*b0+d0*a1*b1+e*x*y ",
+       Trigger       = "TRUE",
+       WhatHappens   = "n_ = n_*(1+(-x-y+e*x*y))",
        Periodicity   = 1,
        Repetitions   = Inf),
-  list(ID="ANTIBIOTICO SOBRE A",
-       Trigger       = "T>1",
-       WhatHappens   = "n_A = n_A-x*n_A*a1-y*n_A*b1+d1*a1*b1+e*x*y",
+  list(ID="ANTIBIOTICO SOBRE R",
+       Trigger       = "TRUE",
+       WhatHappens   = "n_R = n_R*(1+(-x*a-y*b+d*a*b+e*x*y))",
        Periodicity   = 1,
        Repetitions   = Inf),
-  list(ID="ANTIBIOTICO SOBRE B",
-       Trigger       = "T>1",
-       WhatHappens   = "n_B = n_B-x*n_B*a1-y*n_B*b1+d0*a1*b1+e*x*y",
-       Periodicity   = 1,
-       Repetitions   = Inf),
-  list(ID="ANTIBIOTICO SOBRE A,B",
-       Trigger       = "T>1",
-       WhatHappens   = "n_C = n_C-x*n_C*a1-y*n_C*b1+d2*a1*b1+e*x*y",
+  list(ID="ANTIBIOTICO SOBRE R2",
+       Trigger       = "TRUE",
+       WhatHappens   = "n_R2 = n_R2*(1+(-x*a2-y*b2+d*a2*b2+e*x*y))",
        Periodicity   = 1,
        Repetitions   = Inf))
 
-
-
-variables_modelo <- list(
-  list(Name = "a0",
-       Value = 1
+a <- 0.5; b <- 0.2; a2 <- 0.1 ; b2 <- 0.6;  d <- -0.1; 
+e <- -0.5 ;x <- 0.05; y <- 0.05; tiempo <- 3000
+-x*a-y*b+d*a*b+e*x*y
+stopifnot((-x*a-y*b+d*a*b+e*x*y)<0 & (-x*a2-y*b2+d*a2*b2+e*x*y)<0)
+variables <- list(
+  list(Name = "a",
+       Value = a
   ),
-  list(Name = "a1",
-       Value = 0.6
+  list(Name = "a2",
+       Value = a2
   ),
-  list(Name = "b0",
-       Value  = 1
+  list(Name = "b",
+       Value = b
   ),
-  list(Name = "b1",
-       Value = 0.4
+  list(Name = "b2",
+       Value = b2
   ),
   list(Name = "x",
-       Value = 0.5
+       Value = x
   ),
   list(Name = "y",
-       Value = 0.5
+       Value = y
   ),
-  list(Name= "d0",
-       Value= 0
-  ),
-  list(Name= "d1",
-       Value= 1
-  ),
-  list(Name= "d2",
-       Value= -0.1
+  list(Name= "d",
+       Value= d
   ),
   list(Name= "e",
-       Value= -0.1
+       Value= e
   )
 )
 
+RULES <- list(list(ID="CAMBIO ANTIBIOTICO ANTI R2",
+                   Condition="n_R2 > 2*n_R",
+                   Action="x = 0.04 ; y = 0.07"),
+              list(ID="CAMBIO ANTIBIOTICO ANTI R",
+                   Condition="n_R > 2*n_R2",
+                   Action="x = 0.07 ; y = 0.04"))
+vaRiables <- createUserVars(variables)
+inteRvenciones <- createInterventions(intervenciones,afe)
+rules <- createRules(RULES,afe)
 
-
-
-v_Model <- createUserVars(variables_modelo)  
-
-inter2 <- createInterventions(intervenciones,afe2)
-
-simu2 <- oncoSimulIndiv(afe2,
-                        initMutant = c("WT", "B", "A", "C"),
-                        initSize = c(970,10,10,10),
-                        finalTime = 40,
-                        mu=0.0000000001,
-                        interventions = inter2,
-                        userVars = v_Model,
-                        keepEvery = 1)
-plot(simu2,show="genotypes")
-
-pobs <- unlist(simu2$pops.by.time)[,2:5]
-totalpob <- rowSums(unlist(simu2$pops.by.time)[,2:5])
-freqs <- pobs/totalpob
-time <- unlist(simu2$pops.by.time)[,1]
-max <- totalpob/totalpob
-plot(max~time,type="l", col="black",ylim=c(0,1.1),ylab="FREQUENCY")
-lines(time,freqs[,1],type="l",col="#A6761D")
-lines(time,freqs[,2],type="l",col="#666666")
-lines(time,freqs[,3],type="l",col="#1B9E89")
-lines(time,freqs[,4],type="l",col="red")
-
-
-
+#SIMULACIONES
+sim<- oncoSimulIndiv(afe, 
+                     keepEvery = 1,
+                     mu= 0.001,
+                     initMutant= c("WT","R","R2"),
+                     initSize = c("WT"=9900,"R"=100,"R2"=100),
+                     interventions = inteRvenciones,
+                     userVars = vaRiables,
+                     finalTime = tiempo,
+                     rules = rules)
+par(mfrow=c(1,2))
+plot(sim,show = "genotypes")
+N_tot <- rowSums(unlist(sim$pops.by.time)[,2:4])
+tiempo <- unlist(sim$pops.by.time)[,1]
+plot(tiempo,N_tot,
+     col="orange",
+     main="Evolución población total",
+     type="line",
+     ylab="Células",
+     xlab="TIEMPO")
