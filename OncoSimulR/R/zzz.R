5b437d1bdece57fee6c65bfe40b4e196b6978aa4
Ramon Diaz-Uriarte <rdiaz02@gmail.com>
2025-01-10 23:10:01 +0100

    ooops, version numbers and NEWS that should have been committed and pushed a while ago

--- OncoSimulR/R/zzz.R
+++ OncoSimulR/R/zzz.R
@@ -1,12 +1,13 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("This is package ", pkgname, ".",
+  packageStartupMessage("This is package ", pkgname, ". ",
                         "If you are running it on an aarch64 (arm64) ",
                         "platform with a MacOS ",
                         "note that the package fails some tests ",
+                        "in that platform + OS ",
                         "that I have no way of debugging. ",
                         "Please read file ",
                         "README_tests_kjohnson3_aarch64-apple-darwin20.txt ",
-                        "in the tests directory.",
+                        "in the tests directory. ",
                         "As of 2024-10-09, this platform is unsupported ",
                         "until we can properly debug it.")
 }
