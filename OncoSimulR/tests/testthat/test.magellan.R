f8e749ea2579e2a8ccc5238d6f99b54d25d336c3
Ramon @ Triturus <no_specific_email>
2026-04-05 21:05:17 +0200

    minimal magellan psitasis tests

--- OncoSimulR/tests/testthat/test.magellan.R
+++ OncoSimulR/tests/testthat/test.magellan.R
@@ -28,6 +28,42 @@ test_that("Call Magellan stats on RMF", {
     })
 
 
+
+test_that("Minimal Magellan tests", {
+  r1 <- rfitness(2)
+
+  r1_noepi <- r1
+  r1_noepi[, 3] <- c(1, 2, 3, 4)
+
+  r1_sign <- r1
+  r1_sign[, 3] <- c(1, 2, 0.1, 4)
+
+  r1_rsign <- r1
+  r1_rsign[, 3] <- c(1, 3, 4, 2)
+
+  expect_equal(Magellan_stats(r1_noepi)[c("sign", "rsign")],
+               c(sign = 0, rsign = 0))
+  expect_equal(Magellan_stats(r1_sign)[c("sign", "rsign")],
+               c(sign = 1, rsign = 0))
+  expect_equal(Magellan_stats(r1_rsign)[c("sign", "rsign")],
+               c(sign = 0, rsign = 1))
+
+  r2 <- rfitness(3)
+
+  r2[, 4] <- c(1, 3, 1.5, 0.1, 2, 4, 1.2, 1)
+  ## sign:
+  ##    WT, A, B, AB
+  ##    WT, A, C, AC
+  ##    A, AB, AC, ABC
+  ##    B, AB, BC, ABC
+  ## rsign:
+  ##    C, AC, BC, ABC
+
+  expect_equal(Magellan_stats(r2)[c("sign", "rsign")],
+               c(sign = 4/6, rsign = 1/6))
+})
+
+
 cat(paste("\n Ending test.magellan at", date(), "\n"))
 cat(paste("  Took ", round(difftime(Sys.time(), inittime, units = "secs"), 2), "\n\n"))
 rm(inittime)
